545a337041ad005a05f73bb704ab121fa2b88939
Pucp Virtual <soporte-empresas@pucp.pe>
2013-12-18 12:30:54 -0500

    Update block_chat_console.php

--- chat_console/lang/es/block_chat_console.php
+++ chat_console/lang/es/block_chat_console.php
@@ -2,17 +2,17 @@
       // block_online_users.php - created with Moodle 1.7 beta + (2006101003)
 
 
-$string['pluginname'] = 'Chat Console';
-$string['online_users:viewlist'] = 'View list of online users';
-$string['periodnminutes'] = 'last {$a} minutes';
-$string['timetosee'] = 'Remove after inactivity (minutes)';
+$string['pluginname'] = 'Chat de alumnos';
+$string['online_users:viewlist'] = 'Ver lista de usuarios en línea';
+$string['periodnminutes'] = 'Hace {$a} minutos';
+$string['timetosee'] = 'Eliminar después de inactividad (minutes)';
 $string['configtimetosee'] = 'Number of minutes determining the period of inactivity after which a user is no longer considered to be online.';
 $string['highlightcolor'] = 'Highlight color';
 $string['confighighlightcolor'] = 'Color of highlight for user the cursor moves over (expected: color string).';
 $string['historysend'] = 'Send to history period';
 $string['confighistorysend'] = 'Which messages to send to history.';
-$string['historysend_never'] = 'Never';
+$string['historysend_never'] = 'Nunca';
 $string['historysend_everyday'] = 'Day old messages';
-$string['historysend_everydaybegin'] = 'In the beginning of every day';
+$string['historysend_everydaybegin'] = 'Al inicio de cada día';
 
 ?>
